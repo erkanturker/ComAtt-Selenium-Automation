b3671c0dd30962e3d493a25f342ef197d1ce98de
Gulsen Zehra Turker <gulsenzehra@Gulsens-MacBook-Pro.local>
2024-10-29 09:12:32 -0400

    automate add user

---
+++ src/test/java/comAtt/step_definitions/UsersPageSteps.java
@@ -0,0 +1,40 @@
+package comAtt.step_definitions;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class UsersPageSteps {
+    private final WebDriver driver;
+
+    public UsersPageSteps(Hooks hooks){
+        this.driver = hooks.getDriver();
+    }
+
+    @When("User fills out the form to create a new user")
+    public void user_fills_out_the_form_to_create_a_new_user() {
+        driver.findElement(By.id("username")).sendKeys("testUsername");
+        driver.findElement(By.id("password")).sendKeys("12345");
+        driver.findElement(By.id("firstName")).sendKeys("testFirstName");
+        driver.findElement(By.id("lastName")).sendKeys("testLastName");
+        driver.findElement(By.id("email")).sendKeys("test@gmail.com");
+        Select dropdown = new Select(driver.findElement(By.id("role")));
+        dropdown.selectByVisibleText("Teacher");
+        this.driver.findElement(By.cssSelector("button[data-testid='submit']")).click();
+    }
+
+    @Then("verifies that the alert title is {string}")
+    public void verifies_that_the_alert_title_is(String expectedTitle) {
+        WebDriverWait wait  = new WebDriverWait(driver, Duration.ofSeconds(12));
+        WebElement alertTitle = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("div.alert-heading.h4")));
+        Assert.assertEquals(expectedTitle,alertTitle.getText());
+    }
+}
--- src/test/resources/features/UsersPage.feature
+++ src/test/resources/features/UsersPage.feature
@@ -4,3 +4,10 @@ Feature: Users Page
     Given User logs in to the system
     When  User navigates to "Users" page
     Then  User verifies the dashboard title is "Users"
+
+  Scenario: Admin Can create a User
+    Given User logs in to the system
+    When  User navigates to "Users" page
+    Then  User verifies the dashboard title is "Users"
+    When  User fills out the form to create a new user
+    Then  verifies that the alert title is "Success"
